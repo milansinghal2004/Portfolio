25a1ff86c3ced5ae8007665777fb774c1af8000c
Milan Singhal <singhalmilan92@gmail.com>
2026-05-01 09:25:57 +0530

    Fix typescript error in vite config for process.env

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @ts-ignore - process is a Node global
   base: process.env.GITHUB_ACTIONS ? '/Portfolio/' : '/',
   plugins: [react(), tsconfigPaths()],
 })
